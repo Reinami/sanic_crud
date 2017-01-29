2f63aee16e24275ceb35ef766c5cf34d998ea3e8
Typhon <typhonnge@gmail.com>
2017-01-29 01:07:27 -0800

    prepping for pypi

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## Version 0.1
+
+ - 0.1.0 
+  - Released to public
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='sanic_crud',
+    version='0.1.0',
+    url='https://github.com/Typhon66/sanic_crud',
+    license='MIT',
+    author='Typhon',
+    author_email='typhonnge@gmail.com',
+    description='A REST API framework for building CRUD APIs using Sanic and PeeWee',
+    packages=['sanic_crud'],
+    platforms='any',
+    install_requires=[
+        'peewee=2.8.5',
+        'sanic=0.3.0'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sanic api rest crud'
+)
