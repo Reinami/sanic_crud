5c839d659a8f1b69731dd23fb2e6141e53927428
Typhon <Typhon66@users.noreply.github.com>
2017-02-08 23:44:21 -0800

    Update crud_generation.py

--- sanic_crud/crud_generation.py
+++ sanic_crud/crud_generation.py
@@ -14,5 +14,5 @@ def generate_crud(app, model_array):
         SingleResource = type('SingleResource', (BaseResource,), {'model': model})
         CollectionResource = type('CollectionResource', (BaseCollectionResource,), {'model': model})
 
-        app.add_route(SingleResource.as_view(), shortcuts.base_uri + '/<{}:{}>'.format(shortcuts.primary_key_type, shortcuts.primary_key))
+        app.add_route(SingleResource.as_view(), shortcuts.base_uri + '/<{}:{}>'.format(shortcuts.primary_key, shortcuts.primary_key_type))
         app.add_route(CollectionResource.as_view(), shortcuts.base_uri)
