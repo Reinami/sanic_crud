5622eff65c2ebfeed13b1221a711d8f0b649f6ed
Typhon <typhonnge@gmail.com>
2017-02-18 20:15:51 -0800

    upping version

--- sanic_crud/__init__.py
+++ sanic_crud/__init__.py
@@ -1,6 +1,6 @@
 from .crud_generation import generate_crud
 from .config import CrudConfig, ResponseMessages
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __all__ = ['CrudConfig', 'ResponseMessages', 'generate_crud']
